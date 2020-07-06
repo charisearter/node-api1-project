c4494cba54ec21fcd5ade3be9640d5fa6284dc0b
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-07-06 16:31:33 -0500

    post request for users works

--- index.js
+++ index.js
@@ -26,11 +26,37 @@ server.get('/', (req, res) => {
   res.send('<h1>It is working!</h1>');
 });
 
-//GET request for users
+//GET request for users (only array of users)
 
 server.get('/api/users', (req, res) => {
   res.status(200).json(users); //send user list 
 });
+
+//GET request for users by id (specific user)
+
+// server.get('/api/users/:id', (req, res) => {
+//   const id = req.params.id;
+//   users = users.filter(u => users.id !== id);
+//   const thatOne = users.find(users.id === id); //find the user with specific id
+  
+
+//   if(thatOne) {
+//     //if same id
+//     res.status(200).json(thatOne);
+//   }else{
+//     //not found
+//     res.status(404).json({ message: 'No user by that id found' });
+//   }
+// });
+
+//Post users (add a user)
+server.post('/api/users', (req, res) => {
+  const newUser = req.body;
+  newUser.id = shortid.generate();
+  users.push(newUser);
+  res.status(200).json(newUser); //send user list 
+});
+
 
 //server is listening and the port
 const PORT = 8000;
